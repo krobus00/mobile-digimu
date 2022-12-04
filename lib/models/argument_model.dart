ee34717c666537504490ffdb169a593e7e95b99d
Fathan Akram <fathanakram@fathanakram.local>
2022-12-04 17:10:40 +0700

    funtionality

--- lib/models/argument_model.dart
+++ lib/models/argument_model.dart
@@ -1,9 +1,17 @@
+import 'package:digium/models/museum_model.dart';
 import 'package:digium/models/transaction_model.dart';
 
 class TransactionDetailArgument {
   final TransactionModel transaction;
 
   TransactionDetailArgument({required this.transaction});
+}
+
+class TransactionMuseum {
+  final MuseumModel museum;
+
+  TransactionMuseum({required this.museum});
+  
 }
 
 class TicketDetailArgument {
